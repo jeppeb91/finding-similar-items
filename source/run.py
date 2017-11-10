9d72051e7fc9f7f13074cdb3de41557e37de57ea
jeppeb91 <jeppeb91@hotmail.com>
2017-11-10 18:28:06 +0100

    added way out of main loop

--- source/run.py
+++ source/run.py
@@ -10,9 +10,12 @@ def main():
     my_path_to_dir=my_path_to_dir.strip('\n')
     my_docs=document_base.DocumentBase(my_path_to_dir, SHINGLE_SIZE, NUMBER_OF_PERMUTATIONS, TRESHOLD)
     while True:
-        print "enter path to your document"
+        print "enter path to your document, 0 for exit"
         file_path=sys.stdin.readline()
         file_path=file_path.strip('\n')
+        if file_path == "0":
+            print "bye"
+            break
         print "Comparing: " + str(file_path)
         print "With directory: " + str(my_path_to_dir)
         result_set=my_docs.lsh(file_path)
